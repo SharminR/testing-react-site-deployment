01c8786bef96f8d5d6c43eb3e6319701f70253b4
Rahman Sharmin <s1800091@student.businesscollege.fi>
2019-01-03 21:02:31 +0200

    Updates

---
+++ precache-manifest.92a01a87cdb7e1232900d29daddceffe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "086cbbf00486b59507b8",
+    "url": "/t/static/js/runtime~main.086cbbf0.js"
+  },
+  {
+    "revision": "376bf8bda805efe8ddd9",
+    "url": "/t/static/js/main.376bf8bd.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/t/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "376bf8bda805efe8ddd9",
+    "url": "/t/static/css/main.598a923d.chunk.css"
+  },
+  {
+    "revision": "fd114eb8f8caf499b3cd106bef28a8d2",
+    "url": "/t/index.html"
+  }
+];
